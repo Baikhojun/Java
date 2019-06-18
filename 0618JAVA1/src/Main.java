763e0b9ac33df5c13d5e756bee0a393f8b324af9
WhiteJune <WhiteJune@DESKTOP-5BT0SG3>
2019-06-18 09:47:35 +0900

    if 구문 및 삼항연산자 관리

--- 0618JAVA1/src/Main.java
+++ 0618JAVA1/src/Main.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+import java.util.*;
+
 
 public class Main {
-
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		String result = (num >0) ? "양수다" : "양수가 아니다";
+		System.out.println(num + " 은 " + result);
+		
+		if(num > 0)
+		{
+			System.out.println(num + " 은 양수다" );
+		}else
+		{
+			System.out.println(num + " 은 양수가 아니다");
+		}
+	}
 }
